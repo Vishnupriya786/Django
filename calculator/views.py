bc67e2454c29ee266d0307b8d702ccbe1eda88e0
Vishnupriya786 <shakthipriya182000@gmail.com>
2021-05-29 21:04:08 +0530

    Apply DTL

--- calculator/views.py
+++ calculator/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def createHome(request):
-    return HttpResponse ("Hello world!!!");
+    return render (request,'Login.html',{"name":"Vishnu"})
